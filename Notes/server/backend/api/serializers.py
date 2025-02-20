5e0b3916af1fae35ab8eb4d1274f9b3cb1532d79
BisekhKarki <karkidai79@gmail.com>
2025-02-20 23:29:29 +0545

    Backend half Completed

--- Notes/server/backend/api/serializers.py
+++ Notes/server/backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from .models import Notes
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,3 +12,12 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self,validate_data):
         user = User.objects.create_user(**validate_data)
         return user
+    
+
+class NoteSeralizer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = ['id','title','content','created_at',"author"]
+        extra_kwargs = {"author":{"read_only":True}}
+
+    
--- Notes/server/backend/api/views.py
+++ Notes/server/backend/api/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSeralizer
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated,AllowAny
+from .models import Notes
 
 
-# Create your views here.
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSeralizer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Notes.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+        
+class NoteDelete(generics.DestroyAPIView):
+    
+    serializer_class = NoteSeralizer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Notes.objects.filter(author=user)
+
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = User
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
