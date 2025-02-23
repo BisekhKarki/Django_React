cab7f44dd1f0213ae72662d7d647b4c3330ca683
BisekhKarki <karkidai79@gmail.com>
2025-02-23 22:47:05 +0545

    Views For Room Creation created

--- SpotifyController/server/music_controller/api/urls.py
+++ SpotifyController/server/music_controller/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import main
+from .views import RoomView
 
 urlpatterns=[
-    path("home/",main,name="main"),
+    path("room/",RoomView.as_view(),name="view_room"),
 ]
--- SpotifyController/server/music_controller/api/views.py
+++ SpotifyController/server/music_controller/api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
-def main(request):
-    return HttpResponse("Hello")
+# We are able to not only view all the rooms but also create a room on the view below
+class RoomView(generics.CreateAPIView):
+    queryset  = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
 
 
 
