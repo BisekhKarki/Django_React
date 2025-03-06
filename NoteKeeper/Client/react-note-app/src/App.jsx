17b3ec6c0e65016a9bdb1a4524e18cccc704363b
BisekhKarki <karkidai79@gmail.com>
2025-03-06 21:56:46 +0545

    Frontend Navbar completed

---
+++ NoteKeeper/Client/react-note-app/src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/Navbar";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      {/* <h1>Hello World</h1> */}
+    </>
+  );
+}
+
+export default App;
